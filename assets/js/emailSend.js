fce9a22b632bdd811a1b0c4c1edd959209421c67
Adam Radvinsky <radvinskyadam@gmail.com>
2026-03-02 17:33:25 -0800

    email send button now works

--- assets/js/emailSend.js
+++ assets/js/emailSend.js
@@ -1,35 +1,17 @@
-// fetch("http://localhost:8080/send", {
-//   method: "POST",
-//   headers: { "Content-Type": "application/json" },
-//   body: JSON.stringify({ message: "Test from browser" })
-// })
-// .then(res => res.text())
-// .then(console.log)
-// .catch(console.error);
-
-
-// const options = {
-//   method: "POST",
-//   headers: {
-//     'Content-Type' : 'application/json'
-//   },
-//   body: JSON.stringify("blahblah")
-// };
-
-
-//fetch("http://localhost:8080/send", )
-
-async function sendEmail(){
-  console.log(" TRYING TO SEND AN EMAIL")
-
-  const messageText = " HEY IM SENDING AN EMAIL";
+async function sendEmail() {
 
   const url = "http://localhost:8080/send";
 
   try {
-    
-    
 
+
+    console.log(" TRYING TO SEND AN EMAIL")
+    const respons = fetch(url, {
+      method: "POST",
+      body: " bruh hello email text is diff i hope?" ,
+    });
+
+    console.log(" I have sent an email !?")
 
   } catch (error) {
     console.log(error);
